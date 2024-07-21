a1c2fb70fe1dd7c1772beee030cf63b45b621cda
Sandu <sandurares57@gmail.com>
2024-07-21 21:29:07 +0300

    small css changes

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -12,11 +12,11 @@ const Hero = () => {
                     <h1 className="text-5xl text-center font-bold text-yellow-300 animate-pulse">Step Into the Feline Fantasy of CATSINO</h1>
                     <p className="text-white text-center text-lg md:w-2/3 bg-opacity-90 
                  p-4 rounded-xl shadow-xl mt-5">
-        Dare to roll the dice? Scratch your way to victory? It&apos;s not just playtime it&apos;s your chance to claim the throne at Catsino!
+        Dare to roll the dice? Scratch your way to victory? It&apos;s not just playtime - it&apos;s your chance to claim the throne at Catsino!
     </p>
 
     <p className="text-white text-center text-lg md:w-2/3 bg-opacity-20 bg-gray-200 p-4 rounded-xl shadow-2xl">
-        Unleash your luck and get your paws on jackpot joy where every spin could turn into a purrfect win!
+        Unleash your luck and get your paws on jackpot - joy where every spin could turn into a purrfect win!
     </p>
                     <Link href="/games" className="pt-10">
                         <button className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white text-lg md:text-3xl font-semibold py-4 px-6 rounded-full hover:scale-110 transition-transform duration-300 ease-in-out">
